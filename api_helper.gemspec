5946199674ed82fa2f3bbc22bed7882ccf76aa94
Neson <neson@dex.tw>
2015-06-15 17:40:40 +0800

    use simplecov

--- api_helper.gemspec
+++ api_helper.gemspec
@@ -22,6 +22,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake"
   spec.add_development_dependency "appraisal"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "simplecov"
+  spec.add_development_dependency "coveralls"
   spec.add_development_dependency "byebug"
   spec.add_development_dependency "activerecord"
   spec.add_development_dependency "sqlite3"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,30 @@
+require 'simplecov'
+
+SimpleCov.profiles.define :gem do
+  add_filter '/test/'
+  add_filter '/features/'
+  add_filter '/spec/'
+  add_filter '/autotest/'
+
+  add_group 'Binaries', '/bin/'
+  add_group 'Libraries', '/lib/'
+  add_group 'Extensions', '/ext/'
+  add_group 'Vendor Libraries', '/vendor/'
+end
+
+begin
+  require 'rails'
+rescue LoadError
+else
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+end
+
+SimpleCov.start :gem
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'api_helper'
 
